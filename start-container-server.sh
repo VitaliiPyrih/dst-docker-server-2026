1f0edd714fa5d5f01717a0f2498dfbd29b8c36c1
Vitalii Pyrih <vetall995995@gmail.com>
2026-03-13 11:02:18 +0200

    new version

--- start-container-server.sh
+++ start-container-server.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-# Check for game updates before each start. If the game client updates and your server is out of date, you won't be
-# able to see it on the server list. If that happens just restart the containers and you should get the latest version
-/home/dst/steamcmd.sh +@ShutdownOnFailedCommand 1 +@NoPromptForPassword 1 +login anonymous +force_install_dir /home/dst/server_dst +app_update 343050 +quit
+# CLUSTER_NAME and SHARD_NAME are passed via docker-compose environment
+CLUSTER_NAME="${CLUSTER_NAME:-DSTWhalesCluster}"
+SHARD_NAME="${SHARD_NAME:-Master}"
+
+echo "=== Starting: CLUSTER=$CLUSTER_NAME | SHARD=$SHARD_NAME ==="
+
+# Check for game updates before each start
+/home/dst/steamcmd.sh \
+  +@ShutdownOnFailedCommand 1 \
+  +@NoPromptForPassword 1 \
+  +@sSteamCmdForcePlatformType linux \
+  +login anonymous \
+  +force_install_dir /home/dst/server_dst \
+  +app_update 343050 \
+  +quit
 
 echo "=== Starting mod setup ==="
-
-ds_mods_setup="/home/dst/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/dedicated_server_mods_setup.lua"
+ds_mods_setup="/home/dst/.klei/DoNotStarveTogether/${CLUSTER_NAME}/mods/dedicated_server_mods_setup.lua"
 echo "Checking file: $ds_mods_setup"
-if [ -f "$ds_mods_setup" ]
-then
+if [ -f "$ds_mods_setup" ]; then
   echo "File found, copying..."
   cp $ds_mods_setup "$HOME/server_dst/mods/"
 else
   echo "File NOT found!"
 fi
 
-# Copy modoverrides.lua
 echo "=== Copying modoverrides.lua ==="
-modoverrides="$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/mods/modoverrides.lua"
+modoverrides="$HOME/.klei/DoNotStarveTogether/${CLUSTER_NAME}/mods/modoverrides.lua"
 echo "Checking file: $modoverrides"
-if [ -f "$modoverrides" ]
-then
+if [ -f "$modoverrides" ]; then
   echo "File found, copying to Master and Caves..."
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Master/"
-  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/DSTWhalesCluster/Caves/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/${CLUSTER_NAME}/Master/"
+  cp $modoverrides "$HOME/.klei/DoNotStarveTogether/${CLUSTER_NAME}/Caves/"
   echo "Done!"
 else
   echo "File NOT found!"
 fi
 
 cd $HOME/server_dst/bin
-./dontstarve_dedicated_server_nullrenderer -cluster DSTWhalesCluster -shard "$SHARD_NAME"
+./dontstarve_dedicated_server_nullrenderer \
+  -cluster "$CLUSTER_NAME" \
+  -shard "$SHARD_NAME" \
+  -console
